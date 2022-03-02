4571a415ace2a9d5eafd07ffad050205a28c83f6
Mainulhasan05 <mdrifatbd5@gmail.com>
2022-03-02 19:48:28 +0600

    msg

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ var stack=[]
 const convertToPostFix=()=>{
     let input=document.getElementById('input').value
     const inputtext=document.getElementById('inputtext')
+    let answer=document.getElementById('answer')
     input=input.replaceAll(' ','')
     input=input.toUpperCase()
     inputtext.innerText=input
     let length=input.length
-    let answer=''
 
     output=''
     // (A+B)/C-(B-C)
     arr=input.split('');
     for(i=0; i<length; i++){
-        if(arr[i]=='('){
+        if(arr[i]=='(' ||arr[i]=='+'||arr[i]=='-'||arr[i]=='*'||arr[i]=='/'){
             stack.push(arr[i]);
         }
         else if(arr[i]>='A' && arr[i]<='Z'){
             output+=arr[i]
             console.log(output)
         }
+        else if(arr[i]==')'){
+            const operator=stack.pop()
+            output+=operator
+            
+        }
     }
+    answer.innerText=output
 }
 
 const push=(item)=>{
